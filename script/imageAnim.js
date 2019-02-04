61b8c9cd1c5d0117c240ee841bcbc198c3c78f08
Vira Romanko <viraromanko@Viras-MacBook-Pro.local>
2019-02-04 13:42:46 -0500

    Finished drag and drop img

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -13,6 +13,10 @@
 	let puzzleSelectors = document.querySelectorAll("#buttonHolder img");
 
 
+	// get a reference to the drop area
+	let dropZones = document.querySelectorAll('.drop-zone');
+
+
 	//functions go in the middle
 	function createPuzzlePieces(pictureIndex){
 		//generate images here-> need to make 4 (top. left, rignt, bottom)
@@ -21,11 +25,51 @@
 		//
 		// loop through the images refs and generate one for each
 		thePieces.forEach(( piece, index) => {
-			let newPuzzlePiece = `<img id="piece${index}" class="puzzle-image" src="images/${piece+ pictureIndex}.jpg" alt="puzzle piece">`
+			let newPuzzlePiece = `<img id="piece${index}" class="puzzle-image" src="images/${piece+ pictureIndex}.jpg" alt="puzzle piece" draggable>`
 
 			piecesBoard.innerHTML += newPuzzlePiece;
 		});
+
+		initDrag();
 	}
+
+	// drag and drop functunality
+	// this is a 3-step process
+	//1.handle the drag ivent
+	//2. handle the dragover event
+	//3. handle the drop enent
+	//
+	//dragging sets some data referance (an audio, ima sourse, etc)
+	//dragover -> just prevent the default behaviour
+	// on a drop is where the magic happens -> script that behaviour , get the data referance and
+	// do what you need to do with it
+	function initDrag(){
+		piecesBoard.querySelectorAll('img').forEach(img => {
+			img.addEventListener("dragstart", function(e){
+				console.log('draggin...');
+				e.dataTransfer.setData("text/plain", this.id);
+
+			});
+
+		});
+	}
+
+	// handle the drop
+	// 
+	dropZones.forEach(zone => {
+		zone.addEventListener("dragover", function(e){
+			e.preventDefault();
+			console.log('drag over me now!!!');
+		});
+		zone.addEventListener("drop", function(e) {
+			e.preventDefault();
+			console.log('you dropped something on me');
+
+			let piece = e.dataTransfer.getData("text/plain");
+			e.target.appendChild(document.querySelector(`#${piece}`));
+		});
+	});
+
 
    function resetPuzzlePieces() {
    	debugger;
